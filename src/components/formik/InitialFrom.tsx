f45385da4afd0d5bdcf2f7ecec4b5c5925b4fac6
ronyislam502 <ronyislam502@gmail.com>
2024-10-06 19:40:40 +0600

    formik

---
+++ src/components/formik/InitialFrom.tsx
@@ -0,0 +1,22 @@
+import { Formik, Form } from "formik";
+import { ReactNode } from "react";
+
+interface InitialFormProps {
+  initialValues: any;
+  onSubmit: (values: any) => void;
+  children: ReactNode;
+}
+
+const InitialForm = ({
+  initialValues,
+  onSubmit,
+  children,
+}: InitialFormProps) => {
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {() => <Form className="space-y-4">{children}</Form>}
+    </Formik>
+  );
+};
+
+export default InitialForm;
