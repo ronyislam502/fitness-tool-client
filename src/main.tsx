0ab742b233d793a97bf91801758549715300850c
ronyislam502 <ronyislam502@gmail.com>
2024-10-06 13:39:59 +0600

    redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={abc}>
+    <Provider store={store}>
       <RouterProvider router={avb} />
       <ToastContainer />
     </Provider>
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5174/api",
+  }),
+  tagTypes: ["Product"],
+  endpoints: () => ({}),
+});
+
+export default baseApi;
